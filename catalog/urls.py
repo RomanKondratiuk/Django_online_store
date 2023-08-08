c507c4ddab33d330d0f9a7fcc52d9cbee1490a1e
Roman Kondratiuk <roma.kondratiuk@icloud.com>
2023-08-07 21:27:50 -0400

    Added description of addresses

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from catalog.views import contacts, home
 
 urlpatterns = [
-    path('', contacts),
-    path('', home)
+    path('', home),
+    path('contacts/', contacts)
 ]
 
